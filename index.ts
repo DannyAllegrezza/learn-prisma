8cc96b8ad02bc87b48c1fd3e22ea6709c2d7f049
Danny Allegrezza <danny@dannyallegrezza.com>
2022-03-17 23:51:03 -0400

    demo: Query with a navigation property (included)

--- index.ts
+++ index.ts
@@ -6,6 +6,30 @@ async function main() {
   // type safe queries, nice
   const allManufacturers = await prisma.vehicle_manufacturers.findMany();
   allManufacturers.forEach((x) => console.log(x.name));
+
+  getModelsByManufacturerName("Lotus");
+}
+
+async function getModelsByManufacturerName(name: string) {
+  const manufacturer = await prisma.vehicle_manufacturers.findFirst({
+    where: {
+      name: name,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  if (manufacturer !== null) {
+    const modelsForManufacturer = await prisma.vehicle_manufacturers.findUnique({
+      where: { id: manufacturer.id },
+      include: {
+        vehicle_models: true,
+      },
+    });
+
+    console.log(modelsForManufacturer);
+  }
 }
 
 main()
